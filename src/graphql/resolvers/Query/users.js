75a16c3e7bf1927625b8356b43d0497c88f6a634
rikeshshrestha <rikeshshrestha@gmail.com>
2021-01-27 21:49:29 +0545

    MNC1 initial commit working server

---
+++ src/graphql/resolvers/Query/users.js
@@ -0,0 +1,13 @@
+const usersResolver = async () =>{
+    return [
+        {
+            user_id: 13123,
+            user_name: "test",
+            user_email: "test@test.test",
+            user_password: "testsetsetse",
+            active: active
+    }
+    ]
+}
+
+export default usersResolver
