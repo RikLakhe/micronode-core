2722fea26ef6cbf75599eddbf5554d2c17568d68
rikeshshrestha <rikeshshrestha@gmail.com>
2021-03-09 22:08:34 +0545

    update: graphql docker sync up with user module communication

--- src/graphql/resolvers/Query/users.js
+++ src/graphql/resolvers/Query/users.js
@@ -1,13 +1,7 @@
+import UserService from "#root/adaptors/userService";
+
 const usersResolver = async () =>{
-    return [
-        {
-            user_id: 13123,
-            user_name: "test",
-            user_email: "test@test.test",
-            user_password: "testsetsetse",
-            active: active
-    }
-    ]
+    return await UserService.fetchAllUsers();
 }
 
 export default usersResolver
