7238c58495d114abdb08f3e3cdef945b1aa88993
riklakhe <rikeshlakhe@gmail.com>
2021-08-24 02:04:17 +0545

    test console for integration

--- src/server/formatGraphQAErrors.js
+++ src/server/formatGraphQAErrors.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 const formatGraphQLErrors=error=>{
     const errorDetails = _.get(error,"originalError.response.data");
-
+    console.log('here error==>',errorDetails);
     if(errorDetails?.error) {
         return errorDetails.error;
     }else{
